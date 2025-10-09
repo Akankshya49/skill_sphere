89ba2e8c806d576aaf8d77b3cb4f4cc6bd4a7d65
Subhransu <subhransutripathy05@gmail.com>
2025-10-09 23:36:46 +0530

    controller updated

---
+++ backend/controllers/meetingController.js
@@ -0,0 +1,13 @@
+const user = require('../models/user');
+const Meeting = require('../model/meeting');
+const Community = require('../model/community');
+const Project = require('../model/project');
+
+const meetingController = {
+    //New meeting
+    createMeeting: async(req,res)=>{
+        try{
+            
+        }
+    }
+}
